79a39fc5aadc52a3b3b91e8fa836ea86c8ef4977
Martin Hauner <martin.hauner@gmx.net>
2021-01-05 14:55:50 +0100

    simpler

--- src/testInt/groovy/com/github/hauner/openapi/processor/core/processor/test/TestProcessor.groovy
+++ src/testInt/groovy/com/github/hauner/openapi/processor/core/processor/test/TestProcessor.groovy
@@ -1,17 +1,6 @@
 /*
- * Copyright 2019-2020 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright © 2019 https://github.com/openapi-processor/openapi-processor-core
+ * PDX-License-Identifier: Apache-2.0
  */
 
 package com.github.hauner.openapi.processor.core.processor.test
@@ -35,9 +24,6 @@ import io.openapiprocessor.api.OpenApiProcessor
 
 /**
  *  Simple processor for testing.
- *
- *  @author Martin Hauner
- *  @author Bastian Wilhelm
  */
 @Slf4j
 class TestProcessor implements OpenApiProcessor {
