8a5fa9b91ecd1b458b72fdf45b4dd1889e629b5e
Martin Hauner <martin.hauner@gmx.net>
2021-01-06 11:16:17 +0100

    improve javadoc generation

--- src/main/kotlin/io/openapiprocessor/core/writer/java/JavaDocWriter.kt
+++ src/main/kotlin/io/openapiprocessor/core/writer/java/JavaDocWriter.kt
@@ -5,36 +5,89 @@
 
 package io.openapiprocessor.core.writer.java
 
+import io.openapiprocessor.core.model.Endpoint
+import io.openapiprocessor.core.model.EndpointResponse
+import org.commonmark.node.Document
+import org.commonmark.node.Node
+import org.commonmark.node.Paragraph
 import org.commonmark.parser.Parser
+import org.commonmark.renderer.NodeRenderer
+import org.commonmark.renderer.html.CoreHtmlNodeRenderer
+import org.commonmark.renderer.html.HtmlNodeRendererContext
 import org.commonmark.renderer.html.HtmlRenderer
+
+/**
+ * Do not wrap the top level items in (unwanted) paragraphs.
+ */
+class SkipParentWrapperParagraphsRenderer(val context: HtmlNodeRendererContext)
+    : CoreHtmlNodeRenderer(context), NodeRenderer {
+
+    override fun getNodeTypes(): Set<Class<out Node>> {
+        return setOf(Paragraph::class.java)
+    }
+
+    override fun render(node: Node) {
+        if (node.parent is Document) {
+            visitChildren(node)
+        } else {
+            visit(node as Paragraph)
+        }
+    }
+}
 
 /**
  * create javadoc from OpenAPI descriptions.
  */
-class JavaDocWriter {
+open class JavaDocWriter {
 
-    fun convert(description: String): String {
-        val parser = Parser
-            .builder()
-            .build()
+    val parser: Parser = Parser
+        .builder()
+        .build()
 
-        val doc = parser.parse(description)
-        val renderer = HtmlRenderer
-            .builder()
-            .build()
+    val renderer: HtmlRenderer = HtmlRenderer
+        .builder()
+        .nodeRendererFactory { context -> SkipParentWrapperParagraphsRenderer(context) }
+        .build()
 
+    fun convert(endpoint: Endpoint, endpointResponse: EndpointResponse): String {
         var javadoc = "/**\n"
 
-        javadoc += renderer
+        javadoc += convert(endpoint.description)
+        javadoc += "\n"
+
+        if (endpoint.parameters.isNotEmpty())
+            javadoc += " *\n"
+
+        endpoint.parameters.forEach {
+            javadoc += convert(it.description, "@param ${it.name}")
+            javadoc += "\n"
+        }
+
+        javadoc += " */\n"
+        return javadoc
+    }
+
+    private fun convert(description: String?, intro: String? = null): String {
+        if (description.isNullOrEmpty())
+            return ""
+
+        val doc = parser.parse(description)
+
+        var result = ""
+        if (intro != null) {
+            result = intro
+            result += " "
+        }
+
+        result += renderer
             .render(doc)
+
+        return result
             .dropLastWhile { it == '\n' }
             .lineSequence()
             .map {
                 " * $it"
             }.joinToString("\n")
-
-        javadoc += "\n */\n"
-        return javadoc
     }
 
 }
--- src/test/kotlin/io/openapiprocessor/core/writer/java/JavaDocWriterSpec.kt
+++ src/test/kotlin/io/openapiprocessor/core/writer/java/JavaDocWriterSpec.kt
@@ -7,6 +7,9 @@ package io.openapiprocessor.core.writer.java
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.openapiprocessor.core.builder.api.endpoint
+import io.openapiprocessor.core.model.datatypes.StringDataType
+import io.openapiprocessor.core.model.parameters.ParameterBase
 
 class JavaDocWriterSpec: StringSpec({
 
@@ -16,9 +19,8 @@ class JavaDocWriterSpec: StringSpec({
         writer = JavaDocWriter()
     }
 
-    "converts markdown to javadoc comment"  {
-
-        val md = """
+    "converts endpoint description to javadoc comment" {
+        val endpointDescription = """
             *markdown* description with **text**
         
             - one list item
@@ -30,11 +32,64 @@ class JavaDocWriterSpec: StringSpec({
 
             """.trimIndent()
 
-        val html = writer.convert(md)
+        val endpoint = endpoint("/foo") {
+            description(endpointDescription)
+            responses {
+                status("204") {
+                    response()
+                }
+            }
+        }
+
+        val html = writer.convert(endpoint, endpoint.endpointResponses.first())
 
         html shouldBe """
             /**
-             * <p><em>markdown</em> description with <strong>text</strong></p>
+             * <em>markdown</em> description with <strong>text</strong>
+             * <ul>
+             * <li>one list item</li>
+             * <li>second list item</li>
+             * </ul>
+             * <pre><code>code block
+             * </code></pre>
+             */
+
+            """.trimIndent()
+    }
+
+    "converts endpoint parameter description to javadoc @param" {
+        val parameterDescription = """
+            *markdown* description with **text**
+        
+            - one list item
+            - second list item
+        
+            ```
+            code block
+            ```
+
+            """.trimIndent()
+
+        val endpoint = endpoint("/foo") {
+            description("any")
+            parameters {
+                any(object : ParameterBase("foo", StringDataType(),
+                    true, false, parameterDescription ) {})
+            }
+            responses {
+                status("204") {
+                    response()
+                }
+            }
+        }
+
+        val html = writer.convert(endpoint, endpoint.endpointResponses.first())
+
+        html shouldBe """
+            /**
+             * any
+             *
+             * @param foo <em>markdown</em> description with <strong>text</strong>
              * <ul>
              * <li>one list item</li>
              * <li>second list item</li>
