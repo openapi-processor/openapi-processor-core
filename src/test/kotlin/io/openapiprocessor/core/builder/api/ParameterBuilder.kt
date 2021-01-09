aa7ed26a3f14f96278d1bdc622c510f10565ae7c
Martin Hauner <martin.hauner@gmx.net>
2021-01-09 15:51:31 +0100

    cleanup

--- src/test/kotlin/io/openapiprocessor/core/builder/api/ParameterBuilder.kt
+++ src/test/kotlin/io/openapiprocessor/core/builder/api/ParameterBuilder.kt
@@ -1,13 +1,9 @@
 /*
- * Copyright © 2020 https://github.com/openapi-processor/openapi-processor-core
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-core
  * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.core.builder.api
-
-
-//class ParameterData(val required: Boolean, deprecated: Boolean)
-
 
 class ParameterBuilder {
     var required: Boolean = false
@@ -28,33 +24,3 @@ class ParameterBuilder {
 
 }
 
-
-
-
-//import io.openapiprocessor.core.model.Response
-//import io.openapiprocessor.core.model.datatypes.DataType
-//import io.openapiprocessor.core.model.datatypes.NoneDataType
-//import io.openapiprocessor.core.model.EmptyResponse
-//
-//class ResponseBuilder() {
-//    private val responses: MutableList<Response> = mutableListOf()
-//
-//    fun response(contentType: String? = null, dataType: DataType? = null) {
-//        lateinit var response: Response
-//
-//        if(contentType == null && dataType == null)
-//            response = EmptyResponse()
-//        else
-//            response = Response(
-//                contentType ?: "none",
-//                dataType ?: NoneDataType())
-//
-//        responses.add(response)
-//    }
-//
-//    fun build(): List<Response> {
-//        return  responses
-//    }
-//
-//}
-//
