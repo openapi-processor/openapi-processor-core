9300fd4bed4fa968ccd2f41fe419be3b6751debe
Martin Hauner <martin.hauner@gmx.net>
2021-01-10 10:32:37 +0100

    cleanup

--- src/main/kotlin/io/openapiprocessor/core/writer/java/FormattingException.kt
+++ src/main/kotlin/io/openapiprocessor/core/writer/java/FormattingException.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-core
+ * PDX-License-Identifier: Apache-2.0
+ */
+
 package io.openapiprocessor.core.writer.java
 
 class FormattingException(
