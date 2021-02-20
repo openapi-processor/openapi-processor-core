bf4e7545166bcecefa8bf84fdb384ea7bb3262b8
Martin Hauner <martin.hauner@gmx.net>
2021-02-20 19:46:24 +0100

    simpler

--- src/main/kotlin/io/openapiprocessor/core/converter/wrapper/MultiDataTypeWrapper.kt
+++ src/main/kotlin/io/openapiprocessor/core/converter/wrapper/MultiDataTypeWrapper.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2020 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-core
+ * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.core.converter.wrapper
@@ -29,8 +18,6 @@ import io.openapiprocessor.core.model.datatypes.NoneDataType
  *
  * Used to replace the collection wrapper at Responses or RequestBody's with  {@code Flux<>} or
  * similar types.
- *
- * @author Martin Hauner
  */
 class MultiDataTypeWrapper(
     private val options: ApiOptions,
