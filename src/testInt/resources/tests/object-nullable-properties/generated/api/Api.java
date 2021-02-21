08c544523037e3cf6f9eae0e61bbb4e6878308da
Martin Hauner <martin.hauner@gmx.net>
2021-02-21 18:58:59 +0100

    #45, test to initialize "null" property

--- src/testInt/resources/tests/object-nullable-properties/generated/api/Api.java
+++ src/testInt/resources/tests/object-nullable-properties/generated/api/Api.java
@@ -8,10 +8,14 @@ package generated.api;
 import annotation.Mapping;
 import annotation.Parameter;
 import generated.model.Foo;
+import generated.model.Foo2;
 
 public interface Api {
 
     @Mapping("/foo")
     void patchFoo(@Parameter Foo body);
+
+    @Mapping("/foo2")
+    void patchFoo2(@Parameter Foo2 body);
 
 }
