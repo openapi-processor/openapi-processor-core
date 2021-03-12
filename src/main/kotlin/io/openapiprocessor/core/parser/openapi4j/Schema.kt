5545024032b2e8d9d7c4e0be2091e4af0e9e4a55
Martin Hauner <martin.hauner@gmx.net>
2021-03-12 08:33:35 +0100

    simpler

--- src/main/kotlin/io/openapiprocessor/core/parser/openapi4j/Schema.kt
+++ src/main/kotlin/io/openapiprocessor/core/parser/openapi4j/Schema.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2020 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-core
+ * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.core.parser.openapi4j
@@ -21,8 +10,6 @@ import org.openapi4j.parser.model.v3.Schema as O4jSchema
 
 /**
  * openapi4j Schema abstraction.
- *
- * @author Martin Hauner
  */
 class Schema(val schema: O4jSchema) : ParserSchema {
 
