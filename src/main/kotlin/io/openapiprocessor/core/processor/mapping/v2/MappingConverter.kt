75eb6390953c6542237b6e35c98c4ec02e68c06b
Martin Hauner <martin.hauner@gmx.net>
2021-06-05 10:03:16 +0200

    simpler

--- src/main/kotlin/io/openapiprocessor/core/processor/mapping/v2/MappingConverter.kt
+++ src/main/kotlin/io/openapiprocessor/core/processor/mapping/v2/MappingConverter.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2020 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-core
+ * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.core.processor.mapping.v2
@@ -30,8 +19,6 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker
 /**
  *  Converter for the type mapping from the mapping yaml. It converts the type mapping information
  *  into the format used by [io.openapiprocessor.core.converter.DataTypeConverter].
- *
- *  @author Martin Hauner
  */
 class MappingConverter(val mapping: MappingV2) {
     companion object {
