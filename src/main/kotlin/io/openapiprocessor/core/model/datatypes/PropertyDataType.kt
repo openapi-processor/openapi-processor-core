bfb03d1e6e0177ac05f202de031886d3ddd37185
Martin Hauner <martin.hauner@gmx.net>
2021-12-23 12:28:26 +0100

    openapi-processor/openapi-processor-spring#136, fix & improve tests

--- src/main/kotlin/io/openapiprocessor/core/model/datatypes/PropertyDataType.kt
+++ src/main/kotlin/io/openapiprocessor/core/model/datatypes/PropertyDataType.kt
@@ -5,6 +5,8 @@
 
 package io.openapiprocessor.core.model.datatypes
 
+import io.openapiprocessor.core.model.Documentation
+
 /**
  * schema "properties" data type wrapper. readOnly/writeOnly may be different on each use of the
  * same schema as a property in another schema.
@@ -19,6 +21,10 @@ open class PropertyDataType(
         return dataType.getName()
     }
 
+    override fun getTypeName(): String {
+        return dataType.getTypeName()
+    }
+
     override fun getPackageName(): String {
         return dataType.getPackageName()
     }
@@ -26,6 +32,18 @@ open class PropertyDataType(
     override fun getImports(): Set<String> {
         return dataType.getImports()
     }
+
+    override val referencedImports: Set<String>
+        get() = dataType.referencedImports
+
+    override val constraints: DataTypeConstraints?
+        get() = dataType.constraints
+
+    override val deprecated: Boolean
+        get() = dataType.deprecated
+
+    override val documentation: Documentation?
+        get() = dataType.documentation
 
 }
 
