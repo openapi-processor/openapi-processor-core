2af796d80c411722e928ba1898e86062d1b29d74
Martin Hauner <martin.hauner@gmx.net>
2022-02-06 11:06:17 +0100

    split in openapi 3.0/3.1

--- src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
+++ src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
@@ -10,11 +10,13 @@ data class TestSet(val name: String, val openapi: String)
 val ALL_30: List<TestSet> = listOf(
     TestSet("bean-validation", API_30),
     TestSet("bean-validation-allof-required", API_30),
+    TestSet("bean-validation-iterable", API_30),
     TestSet("params-additional", API_30)
 )
 
 val ALL_31: List<TestSet> = listOf(
     TestSet("bean-validation", API_31),
     TestSet("bean-validation-allof-required", API_31),
+    TestSet("bean-validation-iterable", API_31),
     TestSet("params-additional", API_31)
 )
