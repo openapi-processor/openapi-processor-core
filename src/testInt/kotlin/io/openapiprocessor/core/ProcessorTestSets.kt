56674415711accab698c8ecebe4b9ef448fe45ab
Martin Hauner <martin.hauner@gmx.net>
2022-02-06 18:02:46 +0100

    split in openapi 3.0/3.1

--- src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
+++ src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
@@ -17,7 +17,18 @@ val ALL_30: List<TestSet> = listOf(
     TestSet("endpoint-http-mapping", API_30), // framework specific
     TestSet("javadoc", API_30),
     TestSet("method-operation-id", API_30),
-    TestSet("params-additional", API_30)
+    TestSet("model-name-suffix", API_30),
+    TestSet("object-nullable-properties", API_30),
+    TestSet("object-read-write-properties", API_30),
+    TestSet("object-without-properties", API_30),
+    TestSet("params-additional", API_30),
+    TestSet("params-complex-data-types", API_30), // framework specific
+    TestSet("params-endpoint", API_30),
+    TestSet("params-enum", API_30),
+    TestSet("params-path-simple-data-types", API_30), // framework specific
+    TestSet("params-request-body", API_30), // framework specific
+    TestSet("params-request-body-multipart-form-data", API_30), // framework specific
+    TestSet("params-simple-data-types", API_30), // framework specific
 )
 
 val ALL_31: List<TestSet> = listOf(
@@ -30,5 +41,17 @@ val ALL_31: List<TestSet> = listOf(
     TestSet("endpoint-http-mapping", API_31), // framework specific
     TestSet("javadoc", API_31),
     TestSet("method-operation-id", API_31),
-    TestSet("params-additional", API_31)
+    TestSet("model-name-suffix", API_31),
+    TestSet("object-nullable-properties", API_31), // todo
+    TestSet("object-read-write-properties", API_31),
+    TestSet("object-without-properties", API_31),
+    TestSet("params-additional", API_31),
+    TestSet("params-complex-data-types", API_31), // framework specific
+    TestSet("params-endpoint", API_31),
+    TestSet("params-enum", API_31),
+    TestSet("params-path-simple-data-types", API_31), // framework specific
+    TestSet("params-request-body", API_31), // framework specific
+    TestSet("params-request-body-multipart-form-data", API_31), // framework specific
+    TestSet("params-simple-data-types", API_31), // framework specific
+
 )
