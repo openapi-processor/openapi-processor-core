57882f367122413d4e6a006b1377d5b4d0bcb0f3
Martin Hauner <martin.hauner@gmx.net>
2022-02-09 08:13:03 +0100

    cleanup

--- src/testInt/groovy/io/openapiprocessor/core/TestSets.groovy
+++ src/testInt/groovy/io/openapiprocessor/core/TestSets.groovy
@@ -8,31 +8,6 @@ package io.openapiprocessor.core
 class TestSets {
 
     static def ALL = [
-        'ref-array-items-nested',
-        'ref-chain-spring-124.1',
-        'ref-chain-spring-124.2',
-        'ref-into-another-file',
-        'ref-into-another-file-path',
-        'ref-is-relative-to-current-file',
-        'ref-loop',
-        'ref-loop-array',
-        'ref-parameter',
-        'ref-parameter-with-primitive-mapping',
-        'ref-to-escaped-path-name',
-        'response-array-data-type-mapping',
-        'response-complex-data-types',
-        'response-content-multiple-no-content',
-        'response-content-multiple-style-all',
-        'response-content-multiple-style-success',
-        'response-content-single',
-        'response-result-mapping',
-        'response-simple-data-types',
-        'response-single-multi-mapping',
-        'schema-composed',
-        'schema-composed-allof',
-        'schema-composed-allof-notype',
-        'schema-composed-allof-properties',
-        'schema-composed-nested'
     ]
 
 }
